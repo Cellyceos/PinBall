fd388ea55a915161330aaa2507cfdc11c05f037d
Cellyceos <brakir@gmail.com>
2019-07-20 13:36:47 +0300

    Added fields
    Add Puck, Wall Bouncy (physic material), Wall Bumper, Wall Slippery (physics material)

---
+++ Assets/Scripts/GL/Fields/Bumper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Bumper : MonoBehaviour
+{
+    /// <summary>
+    /// How strong will the ball be reflected
+    /// </summary>
+    public float bounceForce = 1.3f;
+
+    /// <summary>
+    /// Will check if a ball collided with this object
+    /// If so, apply a negative force of the first contact point times the bounceForce
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter(Collision collision)
+    {
+        var ball = collision.transform.gameObject;
+
+        if (ball)
+        {
+            ball.GetComponent<Rigidbody>().AddForce(-(collision.contacts[0].normal * bounceForce));
+        }
+    }
+}
