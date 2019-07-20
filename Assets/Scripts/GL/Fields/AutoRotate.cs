c2ba36941fd01e1725f5905e45a4a58643d9fc54
Cellyceos <brakir@gmail.com>
2019-07-20 15:08:22 +0300

    Added auto-rotate script

---
+++ Assets/Scripts/GL/Fields/AutoRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _rotationSpeed = new Vector3(0.0f, 5.0f, 0.0f);
+
+    private void Update()
+    {
+        transform.Rotate(_rotationSpeed * Time.deltaTime);
+    }
+}
