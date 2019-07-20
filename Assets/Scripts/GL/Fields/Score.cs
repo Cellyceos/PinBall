7e11e5b0006cfabb427798977539bbb181791a79
Cellyceos <brakir@gmail.com>
2019-07-20 16:50:07 +0300

    Added Score Field

---
+++ Assets/Scripts/GL/Fields/Score.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Score : MonoBehaviour
+{
+    public int score;
+
+    /// <summary>
+    /// Check if a ball collided with this object
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.GetComponent<Ball>())
+        {
+            //do something
+        }
+    }
+}
