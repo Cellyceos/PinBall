1493a9c1144c6d5bfdb02e3e46c8b7eb1cf99e5b
Cellyceos <brakir@gmail.com>
2019-07-22 00:35:47 +0300

    Added GameOverScreen component

---
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -0,0 +1,51 @@
+﻿using Assets.Scripts.UI;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour, IUserInterface
+{
+    [SerializeField]
+    private Button _restartButton;
+    [SerializeField]
+    private Button _menuButton;
+    [SerializeField]
+    private TextMeshProUGUI _scoreText;
+
+    /// <summary>
+    /// Final Score text format
+    /// </summary>
+    public string scoreTextFormat = "Final Score: {0}";
+
+    public static Action onBackToMenu;
+    public static Action onGameRestart;
+    public static Action onGameRestartAI;
+
+    public void Show()
+    {
+        _restartButton.onClick.AddListener(() => {
+            if (AIHelper.enabled)
+                onGameRestartAI?.Invoke();
+            else
+                onGameRestart?.Invoke();
+        });
+
+        _menuButton.onClick.AddListener(() => onBackToMenu?.Invoke());
+
+        gameObject.SetActive(true);
+    }
+
+    public void SetFinalScore(int score)
+    {
+        _scoreText.text = string.Format(scoreTextFormat, score);
+    }
+
+    public void Hide()
+    {
+        _restartButton.onClick.RemoveAllListeners();
+        _menuButton.onClick.RemoveAllListeners();
+
+        gameObject.SetActive(false);
+    }
+}
