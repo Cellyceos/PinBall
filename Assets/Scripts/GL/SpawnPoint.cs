b27f558e91e834614700bdd9afef87c89036e9bb
Cellyceos <brakir@gmail.com>
2019-07-21 15:41:18 +0300

    Added Power Indicator to UI

--- Assets/Scripts/GL/SpawnPoint.cs
+++ Assets/Scripts/GL/SpawnPoint.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SpawnPoint : MonoBehaviour
 {
+    public static Action<float> onPowerChanged;
+
     /// <summary>
     /// Ball 3D model
     /// </summary>
@@ -12,21 +17,40 @@ public class SpawnPoint : MonoBehaviour
     /// <summary>
     /// Min applied force to ball
     /// </summary>
-    public float minAppliedForce = 20;
+    public float minAppliedForce = 10;
 
     /// <summary>
     /// Max applied force to ball
     /// </summary>
     public float maxAppliedForce = 35;
 
+    /// <summary>
+    /// 
+    /// </summary>
+    public float powerGain = 1.2f;
+    private float _power = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
+        // If the key is down gain power
+        if (Input.GetMouseButton(1))
+        {
+            _power += powerGain * Time.deltaTime;
+        }
+
+        var clampedPower = Mathf.Clamp01(_power);
         if (Input.GetMouseButtonUp(1))
         {
+            var forcePower = minAppliedForce + (clampedPower * (maxAppliedForce - minAppliedForce));
+            _power = 0f;
+
             // Spawn it at the spawnPoint and add force
             var ball = Instantiate(ballModel, transform.position, transform.rotation);
-            ball.GetComponent<Rigidbody>().AddForce(Vector3.forward * maxAppliedForce, ForceMode.VelocityChange);
+            ball.GetComponent<Rigidbody>().AddForce(Vector3.forward * forcePower, ForceMode.VelocityChange);
         }
+
+        // Inform subscribers
+        onPowerChanged?.Invoke(clampedPower);
     }
 }
---
+++ Assets/Scripts/UI/GameScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScreen : MonoBehaviour
+{
+    private Image _powerImage;
+
+    // Use this for initialization
+    void Start()
+    {
+        _powerImage = GetComponentInChildren<Image>();
+        SpawnPoint.onPowerChanged += onPowerChanged;
+    }
+
+    private void OnDestroy()
+    {
+        SpawnPoint.onPowerChanged -= onPowerChanged;
+    }
+
+    public void onPowerChanged(float power)
+    {
+        _powerImage.fillAmount = power;
+    }
+}
