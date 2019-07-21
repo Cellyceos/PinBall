0710031c8406496b554a62141e5efb98abc3556a
Cellyceos <brakir@gmail.com>
2019-07-21 17:41:51 +0300

    Added require components checker

--- Assets/Scripts/GL/Ball.cs
+++ Assets/Scripts/GL/Ball.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Ball : MonoBehaviour
 {
     private Rigidbody _rigidbody;
--- Assets/Scripts/GL/LoseArea.cs
+++ Assets/Scripts/GL/LoseArea.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class LoseArea : MonoBehaviour
 {
     /// <summary>
