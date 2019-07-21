317ed6dbe6a7222f330eb89b735cb40aae9d6983
Cellyceos <brakir@gmail.com>
2019-07-21 17:42:30 +0300

    Refactoring

--- Assets/Scripts/GL/Fields/AutoRotate.cs
+++ Assets/Scripts/GL/Fields/AutoRotate.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class AutoRotate : MonoBehaviour
 {
+    /// <summary>
+    /// Rotation speed on XYZ axis
+    /// </summary>
     [SerializeField]
     private Vector3 _rotationSpeed = new Vector3(0.0f, 5.0f, 0.0f);
 
-    private void Update()
+    void Update()
     {
         transform.Rotate(_rotationSpeed * Time.deltaTime);
     }
--- Assets/Scripts/GL/SpawnPoint.cs
+++ Assets/Scripts/GL/SpawnPoint.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class SpawnPoint : MonoBehaviour
 {
