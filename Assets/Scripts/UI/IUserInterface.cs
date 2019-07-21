ceaf44e0c55fa6dd3d63792a88308d4d1f1a3fa5
Cellyceos <brakir@gmail.com>
2019-07-22 00:34:59 +0300

    Added MainScreen component

---
+++ Assets/Scripts/UI/IUserInterface.cs
@@ -0,0 +1,8 @@
+﻿namespace Assets.Scripts.UI
+{
+    interface IUserInterface
+    {
+        void Show();
+        void Hide();
+    }
+}
---
+++ Assets/Scripts/UI/MainScreen.cs
@@ -0,0 +1,32 @@
+﻿using Assets.Scripts.UI;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainScreen : MonoBehaviour, IUserInterface
+{
+    [SerializeField]
+    private Button _startButton;
+    [SerializeField]
+    private Button _aiButton;
+
+    public static Action onGameStart;
+    public static Action onGameStartAI;
+
+  
+    public void Show()
+    {
+        _startButton.onClick.AddListener(() => onGameStart?.Invoke());
+        _aiButton.onClick.AddListener(() => onGameStartAI?.Invoke());
+
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        _startButton.onClick.RemoveAllListeners();
+        _aiButton.onClick.RemoveAllListeners();
+
+        gameObject.SetActive(false);
+    }
+}
